cca4293fc36cda2ce92495b738762ac1f9f567f4
Ali <ali.ozgur@example.com>
2023-02-20 23:02:12 +0300

    added tailwind elements lib

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
         className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
       >
         <form className="absolute inset-[2px] rounded-[8px] bg-[#28292d] z-[10] form flex flex-col p-20">
-          <h2 className="text-[#ff4b45] text-2xl font-[500] text-center tracking-[0.1em]">
+          <h2 className="text-[#cf2c27] text-2xl font-[500] text-center tracking-[0.1em]">
             Sign Up
           </h2>
           <div className="relative w-[300px] mt-[35px] inputbox">
