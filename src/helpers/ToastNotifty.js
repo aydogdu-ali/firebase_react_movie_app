7353891ecbea65136d9eab3ba09b6a2e69853bf1
Ali <ali.ozgur@example.com>
2023-02-23 03:23:34 +0300

    added toastify

---
+++ src/helpers/ToastNotifty.js
@@ -0,0 +1,34 @@
+import { toast } from "react-toastify";
+
+export const toastWarnNotify = (msg) => {
+  toast.warn(msg, {
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  
+  });
+};
+
+export const toastSuccessNotify = (msg) => {
+  toast.success(msg, {
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    
+  });
+};
+
+export const toastErrorNotify = (msg) => {
+  toast.error(msg, {
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+   
+  });
+};
